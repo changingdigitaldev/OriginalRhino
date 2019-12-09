8ef69e882e86fd0904430eff1504bb7337d3d262
changingdigitaldev <changingdigitaldev@gmail.com>
2019-12-10 00:14:11 +0800

    update service hero section

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,11 +4,34 @@ import AboutLearn from '../components/AboutLearn';
 import Hero from '../components/Hero'; 
 import Quote from '../components/Quote'; 
 
+import computer from '../images/hero/about/computer.png'
+import idea01 from '../images/hero/about/idea-01.png'
+import idea02 from '../images/hero/about/idea-02.png'
+import idea03 from '../images/hero/about/idea-03.png'
+import person01 from '../images/hero/about/person-01.png'
+import person02 from '../images/hero/about/person-02.png'
+import person03 from '../images/hero/about/person-03.png'
+import plant from '../images/hero/about/plant.png'
+import gear01 from '../images/hero/about/gear-01.png'
+import gear02 from '../images/hero/about/gear-02.png'
+import gear03 from '../images/hero/about/gear-03.png'
+import heroabout from '../images/hero/hero-about.svg'
+
 class About extends React.Component {
+	constructor(props) {
+		super(props);
+	}
 	render() {
+		var content = {
+			classModifier: 't-or-ms1-mh-hero-about',
+			pageName: 'About Us',
+			text: 'We’re a team of creatives with a passion for great content. Everyone has something to say. Let’s figure out the best way to say it!',
+			staticImage: heroabout,
+			images: [computer, idea03, person01, person02, person03, idea02, idea01, plant, gear03, gear01, gear02]
+		}
 		return (
 			<React.Fragment> 
-				<Hero/>
+				<Hero content={content}/>
 				<AboutContent/>
 				<AboutLearn/>
 				<Quote/>
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -6,16 +6,37 @@ import Format from '../components/Format';
 import Clients from '../components/Clients'; 
 import Quote from '../components/Quote'; 
 
+import graph01 from '../images/hero/services/graph-01.png';
+import person01 from '../images/hero/services/person-01.png';
+import person02 from '../images/hero/services/person-02.png';
+import plus from '../images/hero/services/plus.png';
+import plant01 from '../images/hero/services/plant-01.png';
+import plant02 from '../images/hero/services/plant-02.png';
+import heroservice from '../images/hero/hero-services.svg';
 
-export default function Home() {
+class Services extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+	render() {
+		var content = {
+			classModifier: 't-or-ms1-mh-hero-services',
+			pageName: 'Our Services',
+			text: 'We need to understand you, your audience & your goals. It all starts with a workshop.',
+			staticImage: heroservice,
+			images: ['', '', graph01, person01, person02, '', '', plus, plant01, plant02]
+		}
 	return ( 
-		<React.Fragment> 
-			<Hero/>
-			<OurService/>
-			<OurWork/>
-			<Format/>
-			<Clients/>
-			<Quote/>
-		</React.Fragment>
-	)
+			<React.Fragment> 
+				<Hero content={content}/>
+				<OurService/>
+				<OurWork/>
+				<Format/>
+				<Clients/>
+				<Quote/>
+			</React.Fragment>
+		)
+	}
 }
+
+export default Services;
